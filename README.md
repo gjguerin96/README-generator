d1269a0dbc028abbee43a1afcc2dc18bd7de447f
Gabriel Guerin <gjguerin96@gmail.com>
2021-11-09 16:24:42 -0800

    added javascript functionality

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# README-generator
+# Portfolio
+
+## Description
+This is a portfolio where I can show my work.
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Just do a git pull.
+
+## Usage
+There are links you can click to see my projects or contact me.
+
+## Contributing
+I made it.
+
+## License
+a
+
+## Badges
+
+
+## Tests
+undefined
+
+## Questions
+If you have any questions, reach out to me at gjg96 or find me on github at gjg.
--- index.js
+++ index.js
@@ -1,13 +1,108 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name: 'github',
+        message: 'Enter github username.'
+    },
+    {
+        type:'input',
+        name: 'email',
+        message: 'Enter email address.'
+    },
+    {
+        type:'input',
+        name: 'title',
+        message: 'Enter project title.'
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: 'Enter project description.'
+    },
+    {
+        type:'input',
+        name: 'installation',
+        message: 'Enter installation instructions.'
+    },
+    {
+        type:'input',
+        name: 'usage',
+        message: 'Enter usage instructions.'
+    },
+    {
+        type:'input',
+        name: 'contribution',
+        message: 'Enter contribution information.'
+    },
+    {
+        type:'input',
+        name: 'test',
+        message: 'Enter testing information.'
+    },
+    {
+        type:'list',
+        name: 'license',
+        message: 'Enter licensing information.',
+        choices: ['a','b','c']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt(questions).then((answers) => {
+    // console.log(answers.title)
+    // console.log(answers.description)
+    // console.log(answers.installation)
+    // console.log(answers.usage)
+    // console.log(answers.contribution)
+    // console.log(answers.test)
+    // console.log(answers.license)
+    const content = `# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contribution}
+
+## License
+${answers.license}
+
+## Badges
+
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, reach out to me at ${answers.email} or find me on github at ${answers.github}.`
+    // console.log(content)
+    fs.writeFile('README.md',content, (error) =>{
+        if(error) throw error;
+        console.log("Success! Saved!")
+    })
+})
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
